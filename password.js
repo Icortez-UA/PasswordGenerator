eefe797bd8fe32a27da6e56ea7d349e76f929059
Ikarus C <56051401+Icortez-UA@users.noreply.github.com>
2019-11-08 16:18:58 -0800

    added confirm ? wrote function for values

--- password.js
+++ password.js
@@ -1,37 +1,38 @@
 document.getElementById("Generate").addEventListener("click",function(){
-    var num1 = ["0","1","2","3","4","5","6","7","8","9"];
-    var length1 = prompt("Choose length of password 8-128?");
-    //var symbols = prompt("Do you want to use symbols? Yes or No?");
-    var finalPass ="";
-    //var letters = prompt("Do you want to use letters?");
-    if(length1<8 || length1>128){
-        alert("enter numbers between 8-128");
+    var passwordEl = document.getElementById('password');
+    var length1 = +prompt("Choose length of password 8-128?");
+    var num1 = confirm("Do you want to use numbers?")
+    var symbols = confirm("Do you want to use symbols?");
+    var upperCaseLetters = confirm("Do you want to use upper case letters?");
+    var lowerCaseLetters = confirm("Do you want to use lower case letters?");
+    finalPass=""; 
+    if(num1,symbols,upperCaseLetters,lowerCaseLetters==true){
+        for(i=0; i< length1; i++){
+            finalPass= finalPass+getLowerCase()+getRandomNum()+getSymbols()+getUpperCase();
+        }
     }
-    else{
-        for(var i=0; i <= length1; i++);
-        finalPass= finalPass+ Math.floor(Math.random()* Math.floor(num1.length-1));
-        //console.log(finalPass);
-    }
-    /* if(symbols ==="Yes"){
-        pass2 = Math.floor(Math.random()*symbolPass.length);
-        pass3 = symbolPass[pass2]
-        console.log(pass3);
-    }*/
-    console.log(finalPass);
-    //write function to combine password and use length user is asking.
+    // Password generator function?
     
     
+    // console log test output
+    var ultraFinal= finalPass.slice(0,length1);
+    console.log(ultraFinal);
+
+    // functions to get random values
+        function getRandomNum(){
+           return Math.floor(Math.random()*10);
+        }
+        function getLowerCase(){
+           var lowerCase2 = "abcdefghijklmnopqrstuvwxyz";
+           return lowerCase2[Math.floor(Math.random()*lowerCase2.length)];
+        }
+        function getUpperCase(){
+           var upperCase2 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+           return upperCase2[Math.floor(Math.random()*upperCase2.length)];
+        }
+        function getSymbols(){
+           var symbol2 = ["~","!","#","$","%"];
+           return symbol2[Math.floor(Math.random()*symbol2.length)];
+        }
 }
 );
-function getSymbols(){
-    var symbolEl = ["~","!","#","$","%"];
-    return symbolEl[Math.floor(Math.random()*symbolEl.length)];
-}
-function getRandomNum(){
-    return Math.floor(Math.random()*10);
-}
-function getLowerCase(){
-    var lowerCase = "abcdefghijklmnopqrstuvwxyz";
-    return lowerCase[Math.floor(Math.random()*lowerCase.length)];
-}
-console.log(getLowerCase());
